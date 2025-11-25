d3d77ae1b00026481005c21714f3e1abb54a32ca
John Johnson <wikidfranchise@gmail.com>
2025-11-25 10:08:55 -0800

    Create App component to render Game component

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './components/Game';
+
+const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <Game />
+    </React.StrictMode>
+  );
+};
+
+export default App;
